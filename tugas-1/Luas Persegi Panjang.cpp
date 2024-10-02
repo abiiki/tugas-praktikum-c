0923a105856f48a6e107f074937f3c98160b3370
abiiki <rizqifauzan912@gmail.com>
2024-10-02 10:56:16 +0700

    first commit

---
+++ tugas-1/Luas Persegi Panjang.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int A, B;
+	A = 10;
+	B = 7;
+	
+	printf( " jika persegi panjang memiliki panjang 10 cm dan lebar 7 cm maka luasnya adalah %i cm",(A*B));
+	
+	return 0
+}
